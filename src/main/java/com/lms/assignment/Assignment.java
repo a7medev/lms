e2c03f3cd3f74325508e375091caca3b2a92decf
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-12-13 18:39:21 +0200

    refactor: use lombok instead of boilerplate

--- src/main/java/com/lms/assignment/Assignment.java
+++ src/main/java/com/lms/assignment/Assignment.java
@@ -5,9 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Assignment {
     @Id
@@ -19,58 +25,4 @@ public class Assignment {
     @NotNull private String title;
     @NotNull private String description;
     @NotNull private LocalDateTime dueDate;
-
-    public Assignment(Long id, String title, String description, LocalDateTime dueDate, Long courseId) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.courseId = courseId;
-    }
-
-    public Assignment(String title, String description, LocalDateTime dueDate, Long courseId) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.courseId = courseId;
-    }
-
-    public Assignment() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDateTime dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Long getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(Long courseId) {
-        this.courseId = courseId;
-    }
 }
