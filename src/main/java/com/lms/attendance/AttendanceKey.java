c75f5a271de8429a711730270488b20b430098ae
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-12-15 18:40:25 +0200

    Use Lombok's auto-generated getters/setters

--- src/main/java/com/lms/attendance/AttendanceKey.java
+++ src/main/java/com/lms/attendance/AttendanceKey.java
@@ -1,45 +1,23 @@
 package com.lms.attendance;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class AttendanceKey implements Serializable {
-    
+
     private Long courseId;
     private Long lessonId;
     private Long studentId;
-
-    public AttendanceKey() {}
-
-    public AttendanceKey(Long courseId, Long lessonId, Long studentId) {
-        this.courseId = courseId;
-        this.lessonId = lessonId;
-        this.studentId = studentId;
-    }
-
-    public Long getCourseId() { return courseId; }
-    public void setCourseId(Long courseId) { this.courseId = courseId; }
-
-    public Long getLessonId() { return lessonId; }
-    public void setLessonId(Long lessonId) { this.lessonId = lessonId; }
-
-    public Long getStudentId() { return studentId; }
-    public void setStudentId(Long studentId) { this.studentId = studentId; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AttendanceKey that = (AttendanceKey) o;
-        return Objects.equals(courseId, that.courseId) &&
-               Objects.equals(lessonId, that.lessonId) &&
-               Objects.equals(studentId, that.studentId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(courseId, lessonId, studentId);
-    }
 }
--- src/main/java/com/lms/attendance/AttendanceRecord.java
+++ src/main/java/com/lms/attendance/AttendanceRecord.java
@@ -1,24 +1,29 @@
 package com.lms.attendance;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "attendance_record", uniqueConstraints = @UniqueConstraint(columnNames = {"course_id", "lesson_id", "student_id"}))
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttendanceRecord {
 
     @EmbeddedId
-    private AttendanceKey id;  
+    private AttendanceKey id;
 
     private boolean attended;
+
+    @CreatedDate
     private LocalDateTime timestamp;
-
-    public AttendanceKey getId() { return id; }
-    public void setId(AttendanceKey id) { this.id = id; }
-
-    public boolean isAttended() { return attended; }
-    public void setAttended(boolean attended) { this.attended = attended; }
-
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
 }
