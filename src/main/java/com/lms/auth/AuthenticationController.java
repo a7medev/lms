464138e9d03f047608f8a564f640c3cfadbc7921
Mahmoud <mahmoudsamymahmoud01@gmail.com>
2024-12-15 23:55:35 +0200

    chore: use validated annotation for requests

--- src/main/java/com/lms/auth/AuthenticationController.java
+++ src/main/java/com/lms/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.lms.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,12 @@ public class AuthenticationController {
     private final AuthenticationService authService;
 
     @PostMapping("/add")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Validated RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Validated AuthenticationRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
 }
