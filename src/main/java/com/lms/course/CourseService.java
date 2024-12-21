aa4b95dbf46315727dc6b7f6d006b68449e060e1
Yousef-Albasel <albaselofficial@gmail.com>
2024-12-22 01:14:25 +0200

    feat: preAuthorize access control rules

--- src/main/java/com/lms/course/CourseService.java
+++ src/main/java/com/lms/course/CourseService.java
@@ -1,19 +1,15 @@
 package com.lms.course;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@RequiredArgsConstructor
 @Service
 public class CourseService {
 
-    final CourseRepository courseRepository;
-    @Autowired
-    public CourseService(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
-    }
+    private final CourseRepository courseRepository;
 
     public List<Course> getAllCourses() {
         return courseRepository.findAll();
