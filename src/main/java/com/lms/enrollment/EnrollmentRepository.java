a2d631cc1c186c7cb2a5a48121b65224a011e2a8
Yousef-Albasel <albaselofficial@gmail.com>
2024-12-23 02:50:30 +0200

    Fix some weird issue that happened while resolving conflicts ._.

--- src/main/java/com/lms/enrollment/EnrollmentRepository.java
+++ src/main/java/com/lms/enrollment/EnrollmentRepository.java
@@ -8,9 +8,6 @@ import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long>{
     List<Enrollment> findAllByCourseCourseId(Long courseId);
-<<<<<<< HEAD
     List<Enrollment> findAllByCourseCourseIdAndEnrollmentState(Long courseId, EnrollmentState state);
-=======
     Collection<Enrollment> findAllByUser(User user);
->>>>>>> 0b12282 (fix: show course details based on user's role)
 }
